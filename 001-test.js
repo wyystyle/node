cd95de0fa8aa8dab8e7e71e942f3e8e895b048f5
yang <1823029696@qq.com>
2018-07-21 18:25:26 +0800

    add

---
+++ 001-test.js
@@ -0,0 +1,14 @@
+
+/*require('./002-test2.js');
+console.log(str1);
+console.log(global);*/
+
+
+let t3=require('./003-test3.js');
+	console.log(t3);
+	console.log(t3.obj1);
+	console.log(t3.fn);
+	// console.log(t3.arr);
+
+
+
